4bc0c6370fcb4b8d256567331e5267c2004ff729
Tushar shivade <tusharshivade122@gmail.com>
2026-07-07 23:49:52 +0530

    Add some funcation example

---
+++ Day02/funcation.py
@@ -0,0 +1,33 @@
+def greet():
+    print("Hello! Welcome to Python.")
+
+# function Calling
+greet()
+
+
+
+
+#here some common example
+def greet_user(name):
+    print(f"Hello, {name}!")
+
+greet_user("Tushar")
+
+
+
+#Function with a Return Value
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Saving the result to a variable
+result = add_numbers(10, 15)
+print(result)
+
+
+
+#Function with Default Arguments
+def introduce(name, country="India"):
+    print(f"{name} is from {country}.")
+introduce("John", "USA")  
+
+introduce("Tushar")       
